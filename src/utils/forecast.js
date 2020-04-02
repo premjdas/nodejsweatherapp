1c88012df7e97aec7b39af09f40f6b69cc54e00e
premjdas <premdas2000@gmail.com>
2020-04-02 12:49:36 +0400

    Initial Commit
    	new file:   .gitignore
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   public/.DS_Store
    	new file:   public/css/styles.css
    	new file:   public/img/me.png
    	new file:   public/img/weather.png
    	new file:   public/js/app.js
    	new file:   src/app.js
    	new file:   src/utils/forecast.js
    	new file:   src/utils/geocode.js
    	new file:   templates/partials/footer.hbs
    	new file:   templates/partials/header.hbs
    	new file:   templates/views/404.hbs
    	new file:   templates/views/about.hbs
    	new file:   templates/views/help.hbs
    	new file:   templates/views/index.hbs
    	new file:   views/about.hbs
    	new file:   views/help.hbs
    	new file:   views/index.hbs

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/033807482b8361fe09f2c1d6a9959769/' + latitude + ',' + longitude
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
